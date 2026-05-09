79a750696b232e3d8a92f5eadaa9c1097b2b2fbc
X-jelly18 <elitegamer128k@gmail.com>
2026-05-09 10:23:51 +0200

    Change TARGET URL from HTTPS to HTTP

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Backend
-const TARGET = "https://free.ayanakojivos.shop";
+const TARGET = "http://free.ayanakojivos.shop";
 
 // Proxy
 const proxy = httpProxy.createProxyServer({
