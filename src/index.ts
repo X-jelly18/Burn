539d0dbc933d6159616214bc33653ec92e9f3219
X-jelly18 <elitegamer128k@gmail.com>
2026-05-10 05:22:19 +0200

    Change proxy target URL in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import httpProxy from "http-proxy";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const TARGET = "https://gift.ayanakojivos.shop";
+const TARGET = "https://cum.ayanakojivos.shop";
 
 const proxy = httpProxy.createProxyServer({
   target: TARGET,
