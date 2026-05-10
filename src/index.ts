4c501517ed910949694005f1c5614692bdee3992
X-jelly18 <elitegamer128k@gmail.com>
2026-05-10 02:21:16 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import httpProxy from "http-proxy";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const TARGET = "wss://uk.sshws.net";
+const TARGET = "wss://gift.ayanakojivps.shop";
 
 const proxy = httpProxy.createProxyServer({
   target: TARGET,
