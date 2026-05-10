17c540a732e7c27708b54d50f498458707ebd5b9
X-jelly18 <elitegamer128k@gmail.com>
2026-05-10 05:38:04 +0200

    Change proxy target URL from HTTPS to HTTP

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import httpProxy from "http-proxy";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const TARGET = "https://cum.ayanakojivps.shop";
+const TARGET = "http://cum.ayanakojivps.shop";
 
 const proxy = httpProxy.createProxyServer({
   target: TARGET,
